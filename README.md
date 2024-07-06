7125e464c425490b4563a0c48790bd09935289a6
PangYuanbo <yuanbopang@outlook.com>
2024-07-06 11:46:55 -0700

    11

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.gradescope.com/courses/20666
+https://sp18.datastructur.es
---
+++ WEEK1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+/*
+all code in java must be inside a class
+ */
